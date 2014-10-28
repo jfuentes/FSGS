ffe6ce186ce590586dcb9f0b3e18a999cc9e741e
Joel Fuentes <joel.fuentes@uci.edu>
2014-10-28 01:12:50 -0700

    thread working

--- src/H.h
+++ src/H.h
@@ -2,16 +2,29 @@
 
 #define INITIAL_SIZE_H 100
 
+
 typedef struct{
 	unsigned int numAttributes;
 	unsigned int numRefutations;
 	unsigned int size;
-	
+	unsigned int Y;
 	BIT_ARRAY * matrix[INITIAL_SIZE_H];
 
+	unsigned int existRef[INITIAL_SIZE_H]; //1 indicates there is a refutation, 0 othercase
 
 }H;
 
-H* createH(unsigned int numAttributes);
+typedef struct {
+  	H *matrixH;
+  	unsigned int from;
+  	unsigned int to;
+  	BIT_ARRAY * refutation;
+} argumentsH;
+
+H* createH(unsigned int numAttributes, unsigned int Y);
+
+void addHi(H *h, BIT_ARRAY * newRef);
 
 void destroyH(H *h);
+
+void printH(H *h);
