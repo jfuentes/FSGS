230bb4319cd69022a32b8c6d5c2591549e26ff14
Joel Fuentes <joel.fuentes@uci.edu>
2014-11-09 02:02:41 -0800

    Rows in H are unit32 types, with bit operations HW

--- src/H.h
+++ src/H.h
@@ -1,15 +1,17 @@
-#include "util/bit_array.h"
+#include <inttypes.h>
 
 #define INITIAL_SIZE_H 100
 #define SEQUENTIAL_ALGORITHM 0
 #define MULTICORE_ALGORITHM 1
+
+typedef uint32_t word_t;
 
 typedef struct{
 	unsigned int numAttributes;
 	unsigned int numRefutations;
 	unsigned int size;
 	unsigned int Y;
-	BIT_ARRAY ** matrix;
+	word_t * matrix;
 	struct node *deletedPositions; //simple list to store deleted position for its reutilization
 	unsigned int firstAvailablePosition;
 }H;
@@ -23,7 +25,7 @@ typedef struct {
   	H *matrixH;
   	unsigned int from;
   	unsigned int to;
-  	BIT_ARRAY * refutation;
+  	word_t X;
 } argumentsH;
 
 int type_algorithm;
@@ -31,10 +33,12 @@ typedef int RC; //for errors
 
 H* createH(unsigned int numAttributes, unsigned int Y);
 
-RC addHi(H *h, BIT_ARRAY * newRef);
+RC addHi(H *h, word_t newRef);
 
 void removeHi(H *h, unsigned int i);
 
 void destroyH(H *h);
 
 void printH(H *h);
+
+void printbits(word_t n, unsigned int length);
