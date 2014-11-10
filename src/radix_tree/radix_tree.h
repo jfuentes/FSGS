4424333daab23f60d825247feadf3999de1ac00e
David Carrillo <david.carrillocis@gmail.com>
2014-11-10 04:38:25 -0800

    Deletion functionality working, not riguourosly tested.

--- src/radix_tree/radix_tree.h
+++ src/radix_tree/radix_tree.h
@@ -3,7 +3,7 @@
 #include <bitset>
 #include <cassert>
 #include <sstream>
-
+#include <algorithm>
 #define DEBUG
 
 #ifdef DEBUG
@@ -19,10 +19,18 @@ using namespace std;
 
 template <class storageType, k_size_t B, k_size_t K> struct BlocksVector {
   static_assert(K >= B, "K, the number of bits in tree, must be larger than B, the number of bits in node.");
-  static_assert(K % B == 0, "K must be a multiple of B.");
-  static_assert(B <= sizeof(storageType)*8, "Number of bits in storage type must have at least B.");
+  static_assert(B <= sizeof(storageType) * 8, "Number of bits in storage type must have at least B.");
+  static_assert(sizeof(storageType) * 8 % B == 0, "Number of bits in storage type must be multiple of B.");
+//  static_assert( (B & (B-1)) == 0, "B must be power of 2.");
 
-  const uint_fast8_t nr_elems_per_block = K / B; // Guaranteed to be an integer
+  static const uint_fast8_t block_bitsize = sizeof(storageType) * 8;
+  static const uint_fast8_t nr_elems_per_block = block_bitsize / B;
+
+  static_assert( (nr_elems_per_block & (nr_elems_per_block-1)) == 0, "nr_elems_per_block must be power of 2.");
+
+  static const storageType elem_mask = (1 << B) - 1; // Mask with B LSB set
+
+  static const storageType offset_mask = (nr_elems_per_block - 1);
 
   vector<storageType> elem_blocks;
   uint_fast8_t elems_size_residual = 0;
@@ -44,11 +52,76 @@ template <class storageType, k_size_t B, k_size_t K> struct BlocksVector {
     } else {
      elem_blocks.push_back(q_st);
     }
-    elems_size_residual = (elems_size_residual + 1) & (B-1);
+    cout << "elem_mask: " << bitset<block_bitsize>(elem_mask) << endl;
+    elems_size_residual = (elems_size_residual + 1) & offset_mask;
   }
 
-  void DeleteElems(vector<uint_fast8_t> & elems) {
-  //  sort(elems.begin(), elems.end());
+  void CopyElemsBetweenBlocks(const uint_fast8_t offset_orig, const uint_fast8_t offset_dest,
+                              const storageType block_orig, storageType & block_dest) {
+
+    cout << "block_orig: " << bitset<block_bitsize>(block_orig);
+    cout << " \tblock_dest: " << bitset<block_bitsize>(block_dest) << " \toff_o: " << (int)offset_orig << " off_d: " << (int)offset_dest << endl;
+
+    // Masks to obtain elems to copy
+    const storageType orig_mask = elem_mask << (offset_orig * B);
+    const storageType dest_mask = elem_mask << (offset_dest * B);
+
+    // Get orig element
+    storageType aux = block_orig & orig_mask;
+    aux >>= (offset_orig * B); // Place it leftmost
+    aux <<= (offset_dest * B); // Place it on destination
+
+    // Write destination
+    block_dest &= ~ dest_mask; // Clean destination
+    block_dest |= aux;         // Copy
+
+  }
+
+  void DeleteElems(vector<size_t> & idxs_elems_to_rm) {
+    if (idxs_elems_to_rm.size() == 0)
+      return
+
+    // Sort indexes elements to remove in ascending order
+    sort(idxs_elems_to_rm.begin(), idxs_elems_to_rm.end());
+
+    // Copy current elements to auxiliar array
+    vector<storageType> aux_elem_blocks(elem_blocks);
+
+    size_t i_dest = idxs_elems_to_rm.front();
+    size_t i_orig = i_dest;
+
+    size_t i_rm = 0;
+
+    const size_t nr_elems_orig = nr_elems();
+    const size_t nr_elems_rm = idxs_elems_to_rm.size();
+
+    cout << bitset<K>(elem_mask) << endl;
+    while(i_orig < nr_elems_orig ) {
+
+      cout << "loop " << i_orig << endl;
+      // Move i_orig until idx next elem to add
+      while(i_rm <  nr_elems_rm && i_orig == idxs_elems_to_rm[i_rm]) {
+        ++i_rm;
+        ++i_orig;
+      }
+      if(i_orig < nr_elems_orig) {
+        cout << "loop_after. orig: " << i_orig << " dest: " << i_dest << " rm: " << i_rm << endl; 
+        CopyElemsBetweenBlocks(i_orig & offset_mask, i_dest & offset_mask,
+                               aux_elem_blocks[i_orig / B], elem_blocks[i_dest / B]);
+        ++i_dest;
+        ++i_orig;
+      }
+    }
+
+    cout << "elems_size_residual: " << (int)elems_size_residual << " \t i_orig: " << i_orig << " \t i_dest: " << i_dest << endl;
+
+    
+    elems_size_residual = (i_dest) & offset_mask;
+
+    // Delete unused blocks
+
+    size_t nr_blocks_rm = (i_orig-1)/B - (i_dest-1) / B;
+    elem_blocks.erase(elem_blocks.end() - nr_blocks_rm, elem_blocks.end());
   }
 };
 
@@ -79,11 +152,11 @@ template <class storageType, k_size_t B, k_size_t K> struct FastTrieNode {
       const auto q_and_b = q_st & block;
 
 #ifdef DEBUG
-      cout << "Block: " << bitset<K>(block) << " \t q_and_b: " <<bitset<K>(q_and_b) << endl;
+      cout << "Block: " << bitset<elems.block_bitsize>(block) << " \t q_and_b: " <<bitset<elems.block_bitsize>(q_and_b) << endl;
 #endif
 
       // Mask with B Least Significant Bits set
-      storageType mask_q_one = (1 << (B)) -1;
+      storageType mask_q_one = (1 << B) -1;
 
       for (uint_fast8_t i = 0; i < elems.nr_elems_per_block; ++i) {
         bool found = false;
@@ -95,7 +168,7 @@ template <class storageType, k_size_t B, k_size_t K> struct FastTrieNode {
         }
         if (found) {
 #ifdef DEBUG
-      cout << "Found. find_type: " << find_type << " query: " << bitset<K>(q_st) << " mask:" << bitset<K>(mask_q_one)  << " offset: " << (int)i << endl;
+      cout << "Found. find_type: " << find_type << " query: " << bitset<elems.block_bitsize>(q_st) << " mask:" << bitset<elems.block_bitsize>(mask_q_one)  << " offset: " << (int)i << endl;
 #endif 
          if( matches != nullptr)
             matches->InsertElem(q_st);
@@ -146,6 +219,7 @@ template <class storageType, k_size_t B, k_size_t K> struct FastTrieNode {
     auto tot_elems = node.elems.nr_elems();
 
     for( auto b : node.elems.elem_blocks) {
+      cout << bitset<sizeof(storageType)*8>(b) << endl;
       for(uint_fast8_t i = 0; i < node.elems.nr_elems_per_block && iter_elems < tot_elems; ++i, ++iter_elems) {
         out << iter_elems << ": \t" << bitset<B>(b) << endl;
         b >>= B;
