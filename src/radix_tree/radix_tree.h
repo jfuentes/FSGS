351566cf185c45af3253b1df8e5a81e0a197f0fd
David Carrillo <david.carrillocis@gmail.com>
2014-11-12 00:30:38 -0800

    Work in progress

--- src/radix_tree/radix_tree.h
+++ src/radix_tree/radix_tree.h
@@ -1,12 +1,81 @@
 #include "common.h"
 #include "blocks_vector.h"
+#include <map>
 
-template <typename StorageType, k_size_t B, k_size_t K> struct BlockRadixTreeNode {
+template <typename StorageType, k_size_t B, k_size_t K> struct BaseBlockRadixTreeNode {
 
   BlocksVector<StorageType, B, K> elems;
 
-  void InsertElement(const Query<K> & q, vector<blocks_vector_index_t> & idx_subsets) {
+  vector<blocks_vector_index_t> aux_elem_idxs;
 
+  bool const containsSubset(const Query<K> & q) const {
+    return elems.template FindElems<true, decltype(elems)::FindType::subset>(q, nullptr);
+  }
+
+  bool const containsSuperset(const Query<K> & q) const {
+    return elems.template FindElems<true, decltype(elems)::FindType::superset>(q, nullptr);
+  }
+
+  bool const findSubsets(const Query<K> & q) const {
+    return elems.template FindElems<false, decltype(elems)::FindType::subset>(q, &aux_elem_idxs);
+  }
+
+  bool const findSupersets(const Query<K> & q) const {
+    return elems.template FindElems<false, decltype(elems)::FindType::superset>(q, &aux_elem_idxs);
+  }
+
+  friend ostream& operator<< (ostream & out, const BlockRadixTreeNode & node) {
+
+    size_t iter_elems = 0;
+    auto tot_elems = node.elems.nr_elems();
+
+    for( auto b : node.elems.elem_blocks) {
+      for(uint_fast8_t i = 0; i < node.elems.nr_elems_per_block && iter_elems < tot_elems; ++i, ++iter_elems) {
+        out << iter_elems << ": \t" << bitset<B>(b) << endl;
+        b >>= B;
+      }
+    }
+    return out;
+  }
+};
+
+template <typename StorageType, k_size_t B, k_size_t K> struct BlockRadixTreeNode: BaseBlockRadixTreeNode<StorageType, B, K> {
+
+  vector<BlockRadixTreeNode*> children;
+
+  void InsertElement(Query<K> & q, vector<blocks_vector_index_t> & idx_subsets) {
+    aux_idx_subsets.clear();
+
+    auto q_st = q.template AsStorageType<StorageType, B, false>();
+
+    // Go one level lower
+    assert(q.k_offset >= B);
+    q.k_offset -= B;
+
+    bool found = findSupersets(q);
+
+    if (found) {
+      for (idx_children : aux_elem_idxs) {
+        children[idx_children]->InsertElement(q);
+      }
+    } else {
+      elems.InsertElem(q_st);
+   
+      if(q.k_offset == 0) {
+        // We've reached leafs
+        auto new_node = new BlockRadixTreeLeaf();
+      } else {
+        auto new_node = new BlockRadixTreeNode();
+      }
+      new_node.InsertElement(q);
+      children.push_back(new_node); 
+    }
+  }
+};
+
+template <typename StorageType, k_size_t B, k_size_t K> struct BlockRadixTreeLeaf: BaseBlockRadixTreeNode<StorageType, B, K> {
+
+  void InsertElement(const Query<K> & q) {
     if ( ! containsSuperset(q) ) {
       elems.template FindElems<false, decltype(elems)::FindType::subset>(q, & idx_subsets);
       auto q_st = q.template AsStorageType<StorageType, B, false>();
@@ -47,7 +116,7 @@ template <typename StorageType, k_size_t K> struct BlockRadixTree {
 
   BlockRadixTreeNode<StorageType, B, K> root;
 
-  vector<blocks_vector_index_t> idx_subsets;
+  map<BlockRadixTreeLeaf<StorageType, B, K> *, vector<blocks_vector_index_t> > idx_subsets;
 
   size_t compact_treshold;
 
