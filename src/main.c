0c4959adbaa215f57c853c305aeb4db69f4e9f46
Joel Fuentes <joel.fuentes@uci.edu>
2015-12-09 07:58:24 -0800

    last changes

--- src/main.c
+++ src/main.c
@@ -11,6 +11,131 @@
  * -11 out of memory
  **/
 
+ /*============================================================================*/
+ /*============================================================================*/
+ /*
+  * Memory usage
+  */
+
+ #if defined(_WIN32)
+ #include <windows.h>
+ #include <psapi.h>
+
+ #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+ #include <unistd.h>
+ #include <sys/resource.h>
+
+ #if defined(__APPLE__) && defined(__MACH__)
+ #include <mach/mach.h>
+
+ #elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+ #include <fcntl.h>
+ #include <procfs.h>
+
+ #elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+ #include <stdio.h>
+
+ #endif
+
+ #else
+ #error "Cannot define getPeakRSS( ) or getCurrentRSS( ) for an unknown OS."
+ #endif
+
+
+
+
+
+ /**
+  * Returns the peak (maximum so far) resident set size (physical
+  * memory use) measured in bytes, or zero if the value cannot be
+  * determined on this OS.
+  */
+ size_t getPeakRSS( )
+ {
+ #if defined(_WIN32)
+ 	/* Windows -------------------------------------------------- */
+ 	PROCESS_MEMORY_COUNTERS info;
+ 	GetProcessMemoryInfo( GetCurrentProcess( ), &info, sizeof(info) );
+ 	return (size_t)info.PeakWorkingSetSize;
+
+ #elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+ 	/* AIX and Solaris ------------------------------------------ */
+ 	struct psinfo psinfo;
+ 	int fd = -1;
+ 	if ( (fd = open( "/proc/self/psinfo", O_RDONLY )) == -1 )
+ 		return (size_t)0L;		/* Can't open? */
+ 	if ( read( fd, &psinfo, sizeof(psinfo) ) != sizeof(psinfo) )
+ 	{
+ 		close( fd );
+ 		return (size_t)0L;		/* Can't read? */
+ 	}
+ 	close( fd );
+ 	return (size_t)(psinfo.pr_rssize * 1024L);
+
+ #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+ 	/* BSD, Linux, and OSX -------------------------------------- */
+ 	struct rusage rusage;
+ 	getrusage( RUSAGE_SELF, &rusage );
+ #if defined(__APPLE__) && defined(__MACH__)
+ 	return (size_t)rusage.ru_maxrss;
+ #else
+ 	return (size_t)(rusage.ru_maxrss * 1024L);
+ #endif
+
+ #else
+ 	/* Unknown OS ----------------------------------------------- */
+ 	return (size_t)0L;			/* Unsupported. */
+ #endif
+ }
+
+
+
+
+
+ /**
+  * Returns the current resident set size (physical memory use) measured
+  * in bytes, or zero if the value cannot be determined on this OS.
+  */
+ size_t getCurrentRSS( )
+ {
+ #if defined(_WIN32)
+ 	/* Windows -------------------------------------------------- */
+ 	PROCESS_MEMORY_COUNTERS info;
+ 	GetProcessMemoryInfo( GetCurrentProcess( ), &info, sizeof(info) );
+ 	return (size_t)info.WorkingSetSize;
+
+ #elif defined(__APPLE__) && defined(__MACH__)
+ 	/* OSX ------------------------------------------------------ */
+ 	struct mach_task_basic_info info;
+ 	mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
+ 	if ( task_info( mach_task_self( ), MACH_TASK_BASIC_INFO,
+ 		(task_info_t)&info, &infoCount ) != KERN_SUCCESS )
+ 		return (size_t)0L;		/* Can't access? */
+ 	return (size_t)info.resident_size;
+
+ #elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+ 	/* Linux ---------------------------------------------------- */
+ 	long rss = 0L;
+ 	FILE* fp = NULL;
+ 	if ( (fp = fopen( "/proc/self/statm", "r" )) == NULL )
+ 		return (size_t)0L;		/* Can't open? */
+ 	if ( fscanf( fp, "%*s%ld", &rss ) != 1 )
+ 	{
+ 		fclose( fp );
+ 		return (size_t)0L;		/* Can't read? */
+ 	}
+ 	fclose( fp );
+ 	return (size_t)rss * (size_t)sysconf( _SC_PAGESIZE);
+
+ #else
+ 	/* AIX, BSD, Solaris, and Unknown OS ------------------------ */
+ 	return (size_t)0L;			/* Unsupported. */
+ #endif
+ }
+
+ /*============================================================================*/
+ /*============================================================================*/
+
 /**
  * methods
  **/
@@ -394,6 +519,7 @@ int main(int argc, char* argv[]) {
 
 	rc = readDataSet(relation, nameRelation, numAttributes, numTuples,
 			delimiter);
+   size_t currentSize1 = getCurrentRSS( );
 
 	if (rc < 0) {
 		printf("Error loading the dataset: %d", rc);
@@ -410,9 +536,9 @@ int main(int argc, char* argv[]) {
 			H * matrixH = createH(numAttributes, k);
 			findRefutations(relation, matrixH, k, numAttributes, numTuples);
 			contRef+=matrixH->numRefutations;
-			//printH(matrixH);
+			printf("\nsize %d", sizeof(matrixH));
 			destroyH(matrixH);
-			//break;
+			break;
 			//return 0;
 		}
 	} else {
@@ -421,7 +547,11 @@ int main(int argc, char* argv[]) {
 			rc = findRefutationsMulti(relation, matrixH, k, numAttributes,numTuples);
 		}
 	}
+
+	size_t currentSize2 = getCurrentRSS( );
 	printf("\nTotal refutations found %d", contRef);
 	printf("\n%.5f\n", (double)(time(NULL) - start));
+
+	printf("\nTotal size in bytes %d\n", (currentSize2-currentSize1));
 	return rc;
 }
--- src/radix_tree/main.cc
+++ src/radix_tree/main.cc
@@ -9,12 +9,140 @@
 #include <string.h>
 #include <sys/time.h>
 
+
 using namespace std;
 #define NUMBER_THREADS 16
 
+
+/*============================================================================*/
+/*============================================================================*/
+/*
+ * Memory usage
+ */
+
+#if defined(_WIN32)
+#include <windows.h>
+#include <psapi.h>
+
+#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+#include <unistd.h>
+#include <sys/resource.h>
+
+#if defined(__APPLE__) && defined(__MACH__)
+#include <mach/mach.h>
+
+#elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+#include <fcntl.h>
+#include <procfs.h>
+
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+#include <stdio.h>
+
+#endif
+
+#else
+#error "Cannot define getPeakRSS( ) or getCurrentRSS( ) for an unknown OS."
+#endif
+
+
+
+
+
+/**
+ * Returns the peak (maximum so far) resident set size (physical
+ * memory use) measured in bytes, or zero if the value cannot be
+ * determined on this OS.
+ */
+size_t getPeakRSS( )
+{
+#if defined(_WIN32)
+	/* Windows -------------------------------------------------- */
+	PROCESS_MEMORY_COUNTERS info;
+	GetProcessMemoryInfo( GetCurrentProcess( ), &info, sizeof(info) );
+	return (size_t)info.PeakWorkingSetSize;
+
+#elif (defined(_AIX) || defined(__TOS__AIX__)) || (defined(__sun__) || defined(__sun) || defined(sun) && (defined(__SVR4) || defined(__svr4__)))
+	/* AIX and Solaris ------------------------------------------ */
+	struct psinfo psinfo;
+	int fd = -1;
+	if ( (fd = open( "/proc/self/psinfo", O_RDONLY )) == -1 )
+		return (size_t)0L;		/* Can't open? */
+	if ( read( fd, &psinfo, sizeof(psinfo) ) != sizeof(psinfo) )
+	{
+		close( fd );
+		return (size_t)0L;		/* Can't read? */
+	}
+	close( fd );
+	return (size_t)(psinfo.pr_rssize * 1024L);
+
+#elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
+	/* BSD, Linux, and OSX -------------------------------------- */
+	struct rusage rusage;
+	getrusage( RUSAGE_SELF, &rusage );
+#if defined(__APPLE__) && defined(__MACH__)
+	return (size_t)rusage.ru_maxrss;
+#else
+	return (size_t)(rusage.ru_maxrss * 1024L);
+#endif
+
+#else
+	/* Unknown OS ----------------------------------------------- */
+	return (size_t)0L;			/* Unsupported. */
+#endif
+}
+
+
+
+
+
+/**
+ * Returns the current resident set size (physical memory use) measured
+ * in bytes, or zero if the value cannot be determined on this OS.
+ */
+size_t getCurrentRSS( )
+{
+#if defined(_WIN32)
+	/* Windows -------------------------------------------------- */
+	PROCESS_MEMORY_COUNTERS info;
+	GetProcessMemoryInfo( GetCurrentProcess( ), &info, sizeof(info) );
+	return (size_t)info.WorkingSetSize;
+
+#elif defined(__APPLE__) && defined(__MACH__)
+	/* OSX ------------------------------------------------------ */
+	struct mach_task_basic_info info;
+	mach_msg_type_number_t infoCount = MACH_TASK_BASIC_INFO_COUNT;
+	if ( task_info( mach_task_self( ), MACH_TASK_BASIC_INFO,
+		(task_info_t)&info, &infoCount ) != KERN_SUCCESS )
+		return (size_t)0L;		/* Can't access? */
+	return (size_t)info.resident_size;
+
+#elif defined(__linux__) || defined(__linux) || defined(linux) || defined(__gnu_linux__)
+	/* Linux ---------------------------------------------------- */
+	long rss = 0L;
+	FILE* fp = NULL;
+	if ( (fp = fopen( "/proc/self/statm", "r" )) == NULL )
+		return (size_t)0L;		/* Can't open? */
+	if ( fscanf( fp, "%*s%ld", &rss ) != 1 )
+	{
+		fclose( fp );
+		return (size_t)0L;		/* Can't read? */
+	}
+	fclose( fp );
+	return (size_t)rss * (size_t)sysconf( _SC_PAGESIZE);
+
+#else
+	/* AIX, BSD, Solaris, and Unknown OS ------------------------ */
+	return (size_t)0L;			/* Unsupported. */
+#endif
+}
+
+/*============================================================================*/
+/*============================================================================*/
+
+
 typedef uint32_t word_t;
 typedef uint32_t file_storage_t;
-const k_size_t K = 16;
+const k_size_t K = 32;
 
 struct info{
    int from;
@@ -276,6 +404,7 @@ int main (int argc, char* argv[]) {
    }
 
    rc = readDataSet(nameRelation, delimiter);
+   size_t currentSize1 = getCurrentRSS( );
 
    if (rc < 0) {
       printf("Error loading the dataset: %d", rc);
@@ -287,7 +416,7 @@ int main (int argc, char* argv[]) {
       tree[k] = BlockRadixTree<file_storage_t, K>();
    }
 
-  clock_gettime(CLOCK_MONOTONIC, &start);
+  //clock_gettime(CLOCK_MONOTONIC, &start);
 
    if(typeAlgorithm==0){
       for (int k = numAttributes - 1; k >= 0; k--) {
@@ -295,7 +424,7 @@ int main (int argc, char* argv[]) {
          findRefutations(k);
          tree[k].Compact(0);
          cout << "\nFinished. Rows: " << tree[k].root.elems.nr_elems() << endl;
-
+         break;
       }
    }else{
       if(typeAlgorithm==1){
@@ -315,9 +444,13 @@ int main (int argc, char* argv[]) {
       for (auto& th : threads) th.join();
    }
 }
-   clock_gettime(CLOCK_MONOTONIC, &finish);
-   elapsed = finish.tv_sec - start.tv_sec;
-   elapsed += (finish.tv_nsec - start.tv_nsec)/1000000000.0;
-   cout << "Time: " << elapsed << endl;
+   //clock_gettime(CLOCK_MONOTONIC, &finish);
+   //elapsed = finish.tv_sec - start.tv_sec;
+   //elapsed += (finish.tv_nsec - start.tv_nsec)/1000000000.0;
+   //cout << "Time: " << elapsed << endl;
    //cout << "Time: " << timer.elapsed() << endl;
+
+   size_t currentSize2 = getCurrentRSS( );
+
+   cout << "total Size in bytes: " << (currentSize2 - currentSize1) << endl;
 }
